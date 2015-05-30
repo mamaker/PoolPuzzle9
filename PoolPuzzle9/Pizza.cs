1fc73a87433b1f7cf03f9f3a3ff9910051cf0f6e
mamaker <madhuvasudevan@hotmail.com>
2015-05-29 22:08:11 -0500

    All Done

---
+++ PoolPuzzle9/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PoolPuzzle9
+{
+    class Pizza
+    {
+        private StreamWriter writer;
+        public Pizza(StreamWriter writer)
+        {
+            this.writer = writer;
+        }
+        public void Idaho(Pineapple.Fargo f)
+        {
+            writer.WriteLine(f);
+            writer.Close();
+        }
+    }
+}
